5fc27995d669b53710d02ffbe35c7c0b1179d4ca
Adrian Albin-Clark <a.albin-clark@lancaster.ac.uk>
2017-02-10 10:03:15 +0000

    refactor extraction

---
+++ lib/puree/extractor/base_extractor.rb
@@ -0,0 +1,73 @@
+module Puree
+
+  # Extractor
+  #
+  module Extractor
+
+    # Extractor base
+    #
+    class Base
+
+      def initialize(resource_type:, xml:)
+        @resource_type = resource_type
+        @api_map = Puree::Map.new.get
+        make_doc xml
+      end
+
+      # content based
+      def xpath_query(path)
+        path_from_root = service_xpath path
+        @doc.xpath path_from_root
+      end
+
+      def xpath_query_for_single_value(path)
+        xpath_result = xpath_query path
+        xpath_result ? xpath_result.text.strip : ''
+      end
+
+      def xpath_query_for_multi_value(path)
+        xpath_result = xpath_query path
+        arr = []
+        xpath_result.each { |i| arr << i.text.strip }
+        arr.uniq
+      end
+
+      private
+
+      def make_doc(xml)
+        @doc = Nokogiri::XML xml
+        @doc.remove_namespaces!
+      end
+
+      # Is there any data after get? For a response that provides a count of the results.
+      # @return [Boolean]
+      def get_data?
+        path = service_xpath_count
+        xpath_result = @doc.xpath path
+        xpath_result.text.strip === '1' ? true : false
+      end
+
+      def service_name
+        @api_map[:resource_type][@resource_type][:service]
+      end
+
+      def service_response_name
+        @api_map[:resource_type][@resource_type][:response]
+      end
+
+      def service_xpath_base
+        service_response_name + '/result/content'
+      end
+
+      def service_xpath_count
+        service_response_name + '/count'
+      end
+
+      def service_xpath(str_to_find)
+        service_xpath_base + str_to_find
+      end
+    end
+
+  end
+
+end
