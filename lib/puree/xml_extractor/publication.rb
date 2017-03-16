8f8a2c6286a09f258c26f64cca238d59ea45a627
Adrian Albin-Clark <a.albin-clark@lancaster.ac.uk>
2017-03-16 09:30:03 +0000

    add publication file license

--- lib/puree/xml_extractor/publication.rb
+++ lib/puree/xml_extractor/publication.rb
@@ -40,14 +40,21 @@ module Puree
 
       # @return [Array<Puree::Model::File>]
       def files
-        xpath_result = xpath_query '/electronicVersionAssociations/electronicVersionFileAssociations/electronicVersionFileAssociation/file'
+        xpath_result = xpath_query '/electronicVersionAssociations/electronicVersionFileAssociations/electronicVersionFileAssociation'
         docs = []
         xpath_result.each do |d|
           model = Puree::Model::File.new
-          model.name = d.xpath('fileName').text.strip
-          model.mime = d.xpath('mimeType').text.strip
-          model.size = d.xpath('size').text.strip.to_i
-          model.url = d.xpath('url').text.strip
+          model.name = d.xpath('file/fileName').text.strip
+          model.mime = d.xpath('file/mimeType').text.strip
+          model.size = d.xpath('file/size').text.strip.to_i
+          model.url = d.xpath('file/url').text.strip
+          document_license = d.xpath('licenseType')
+          if !document_license.empty?
+            license = Puree::Model::CopyrightLicense.new
+            license.name = document_license.xpath('term/localizedString').text.strip
+            license.url = document_license.xpath('description/localizedString').text.strip
+            model.license = license if license.data?
+          end
           docs << model
         end
         docs.uniq { |d| d.url }
