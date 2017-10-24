cef24023456e3d960cc026888019223ec90cfced
Adrian Albin-Clark <a.albin-clark@lancaster.ac.uk>
2017-10-24 13:06:26 +0100

    tests

--- spec/query/person_http_spec.rb
+++ spec/query/person_http_spec.rb
@@ -25,6 +25,28 @@ describe 'Person' do
       expect(publications).to all( be_a Puree::Model::Publication )
     end
 
+    it '#publications_offset' do
+      publications = @q.publications uuid: @person_uuid,
+                                     limit: 3,
+                                     offset: 1
+      expect(publications).to all( be_a Puree::Model::Publication )
+    end
+
+    it '#publications_published_start' do
+      publications = @q.publications uuid: @person_uuid,
+                                     limit: 3,
+                                     published_start: '2017-01-01'
+      expect(publications).to all( be_a Puree::Model::Publication )
+    end
+
+    it '#publications_published_start_published_end' do
+      publications = @q.publications uuid: @person_uuid,
+                                     limit: 3,
+                                     published_start: '2017-01-01',
+                                     published_end: '2017-12-31'
+      expect(publications).to all( be_a Puree::Model::Publication )
+    end
+
   end
 
 end
